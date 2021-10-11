1a8b68088aa0c4ef713f44b27a9c63efe1142fed
Patrick Brennan <patbrennan7@gmail.com>
2021-10-11 16:52:29 -0400

    second commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import Button from './Button'
+
+const Header = ({ title, onAdd, showAdd }) => {
+    const onClick = () => {
+        console.log('Click')
+    }
+
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={ onAdd } />
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'task manager',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+//* CSS in JS
+//?<header style={headingStyle}> to replace <header>
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor:'black'
+// }
+
+export default Header
